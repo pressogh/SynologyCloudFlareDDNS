7deead9ca8f3b3da5d6fe3e2da8895c88370a71c
PRESSO <caff1nepill@gmail.com>
2024-05-27 13:32:48 +0900

    Update setddns.py

--- setddns.py
+++ setddns.py
@@ -2,7 +2,7 @@ import configparser
 import urllib.request
 import os, stat
 
-url = 'https://raw.githubusercontent.com/namukcom/SynologyCloudflareDDNS/master/cloudflare.php'
+url = 'https://raw.githubusercontent.com/pressogh/SynologyCloudflareDDNS/master/cloudflare.php'
 target_file = '/usr/syno/bin/ddns/cloudflare.php'
 
 config= configparser.ConfigParser()
